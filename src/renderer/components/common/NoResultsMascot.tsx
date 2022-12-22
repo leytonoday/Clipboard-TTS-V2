96846c8ee54e19475d7226695d06a6b631be765f
Leyton O'Day <leyton.o'day@koder.ly>
2022-12-22 13:35:38 +0000

    Fixed repeating issue with NoResultsMascot gif corrupting. Now just a png

--- src/renderer/components/common/NoResultsMascot.tsx
+++ src/renderer/components/common/NoResultsMascot.tsx
@@ -1,14 +1,14 @@
 import { VStack, Center, Image } from "@chakra-ui/react";
 // @ts-ignore
-import NoResultsMascotGif from "renderer/assets/mascot-no-results.gif"
+import NoResultsMascotGif from "renderer/assets/mascot-no-results.png"
 
 const NoResultsMascot = () => {
   return (
     <VStack justifyContent="center" alignItems="center">
-      <Center marginBottom="-3em">
+      <Center marginBottom="-2em">
         No results
       </Center>
-      <Image onDragStart={(e) => e.preventDefault() } src={NoResultsMascotGif} height="15em" width="15em" />
+      <Image onDragStart={(e) => e.preventDefault() } src={NoResultsMascotGif} height="auto" width="15em" />
     </VStack>
   )
 }
